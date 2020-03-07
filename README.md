0b650028f6260b971a136171a61e110bc8562602
WrightS95 <60717800+WrightS95@users.noreply.github.com>
2020-03-06 22:26:29 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RockPaperScissors
+ basics of python 
---
+++ app.py
@@ -0,0 +1,64 @@
+#rock, paper, scissors
+
+#no matter your choice, computer chooses a random one
+#rock = rock
+#rock<paper
+#rock>scissors...and so on
+
+import random
+choices = ["Rock", "Paper", "Scissors"]
+
+playing = input("Would you like to play? Y/N ")
+
+while playing == "Y":
+    user_choice = input("Rock, Paper, or Scissors?  ")
+    computer_choice = random.choice(choices)
+    if user_choice == "N":
+        print("Bye!")
+        break
+    if user_choice == "Rock":  # --------------user choice = rock
+        print(f"{computer_choice}")
+        if computer_choice == "Rock":
+            print("Tie!")
+        if computer_choice == "Scissors":
+            print("You win!")
+        if computer_choice == "Paper":
+            print("You lose!")
+        if user_choice == "N":
+            print("Bye!")
+            break
+    if user_choice == "Paper": # --------------user choice = paper
+        print(f"{computer_choice}")
+        if computer_choice == "Rock":
+            print("You win!")
+        if computer_choice == "Scissors":
+            print("You lose!")
+        if computer_choice == "Paper":
+            print("Tie!")
+        if user_choice == "N":
+            print("Bye!")
+            break
+    if user_choice == "Scissors": # --------------user choice = scissors
+        print(f"{computer_choice}")
+        if computer_choice == "Rock":
+            print("You lose!")
+        if computer_choice == "Scissors":
+            print("You tie!")
+        if computer_choice == "Paper":
+            print("You win!")
+        if user_choice == "N":
+            print("Bye!")
+            playing = False
+            break
+else:
+    print("Your loss!")
+
+
+
+
+
+
+
+
+
+
